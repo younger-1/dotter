998da77904c33088684a0d76030de34af5e83f3a
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-10-30 01:27:14 +0800

    [pwsh] path var

--- PowerShell/my-alias.ps1
+++ PowerShell/my-alias.ps1
@@ -17,24 +17,31 @@ $localData = "$HOME\AppData\Local"
 # $env:APPDATA
 $roamData = "$HOME\AppData\Roaming"
 
+$config = "$HOME\.config"
+$share = "$HOME\.local\share"
+$bin = "$HOME\.local\bin"
+
+# Scoop
 $scoop = $env:SCOOP
-# Note: $scoop is using backslash: C:\Users\younger\scoop
 $apps = "$scoop\apps"
 $cache = "$scoop\cache"
 $bucket = "$scoop\buckets"
 $shims = "$scoop\shims"
 $persist = "$scoop\persist"
 
-$config = "$HOME/.config"
-$icon = "$HOME/Beauty/Icons"
-$code = "$roamData/Code/User"
-$wt = "$localData/Microsoft/Windows Terminal/"
-$nvim = "$localData/nvim"
+# App
+$wt = "$localData\Microsoft\Windows Terminal"
+$code = "$roamData\Code\User"
+$nvim = "$localData\nvim"
+$packer = "$localData\nvim-data\site\pack\packer"
+$lvim = "$config\lvim"
+$lunar = "$share\lunarvim"
+$lpacker = "$lunar\site\pack\packer"
 
-$notes = "$HOME/desktop/Notes"
-$math = "C:\Users\didi\Desktop\数模\2020数学建模试题\2020年C题--面向康复工程的脑电信号分析和判别模型\附件1-P300脑机接口数据\S1"
-$book = "~/Documents/0-Book and Materials/"
-
+# Other
+$icon = "$HOME\Beauty\Icons"
+$notes = "$HOME\desktop\Notes"
+$book = "$HOME\Documents\0-Book and Materials"
 
 # [File]
 $HOSTS = "$env:SystemRoot\system32\drivers\etc\hosts";
@@ -42,7 +49,7 @@ $AHK = "$config/AutoHotkeyU64.ahk"
 
 # [env]
 # qv2ray
-$env:QV2RAY_CONFIG_PATH = "$config/qv2ray"
+$env:QV2RAY_CONFIG_PATH = "$config\qv2ray"
 # ripgrep
-$env:RIPGREP_CONFIG_PATH = "$config/rgrc"
+$env:RIPGREP_CONFIG_PATH = "$config\rgrc"
 
