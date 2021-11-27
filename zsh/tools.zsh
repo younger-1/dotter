1a507d8ddc439e7a355cb481985d7dda0f7a957d
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-11-27 11:44:07 +0800

    [zsh]: compdef

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -6,13 +6,29 @@ autoload compinit
 compinit
 
 # <https://hacdias.com/2021/05/30/cd-alias-completions-zsh>
-function cdc() {
-  cd "$_config/$1"
-}
+# function cdc() {
+#   cd "$_config/$1"
+# }
 
-function _cdc() {
-  ((CURRENT == 2)) &&
-  _files -/ -W "$_config"
-}
+# function _cdc() {
+#   ((CURRENT == 2)) && _files -/ -W "$_config"
+# }
 
-compdef _cdc cdc
+typeset -A cd_aliases
+cd_aliases=(
+  [cdc]="$HOME/.config"
+  [cds]="$HOME/.local/share"
+  [cdu]="/usr/local"
+)
+
+for k in "${(@k)cd_aliases}"; do
+  function $k() {
+    cd "$cd_aliases[$0]/$1"
+  }
+
+  function _$k() {
+    ((CURRENT == 2)) && _files -/ -W $cd_aliases[${0:1}]
+  }
+
+  compdef _$k $k
+done
