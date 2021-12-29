d080c2c6eb36280799789ed6841b360a753f03c9
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-12-29 13:45:58 +0800

    [linux] proxy clash

--- zsh/boot.sh
+++ zsh/boot.sh
@@ -9,16 +9,15 @@ fi
 # Proxy
 export ClientIP=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -f 1 -d '/')
 export HostIP=$(cat /etc/resolv.conf | grep 'nameserver' | awk '{print $2}')
-export HttpPort=10809
-export SocksPort=10808
+export HttpPort=7890
+# export SocksPort=10808
 
 showproxy() {
     echo ''
-    echo 'Show Proxy:'
     echo "http_proxy=$http_proxy"
     echo "https_proxy=$https_proxy"
-    echo "ftp_proxy=$ftp_proxy"
-    echo "ALL_PROXY=$ALL_PROXY"
+    # echo "ftp_proxy=$ftp_proxy"
+    # echo "ALL_PROXY=$ALL_PROXY"
     echo ''
     echo "curl --connect-timeout 2 baidu.com"
     curl --connect-timeout 5 baidu.com
@@ -28,27 +27,36 @@ showproxy() {
 }
 
 setproxy() {
-    local ad="$1"
+    local ad="${1:-$HostIP:$HttpPort}"
     export http_proxy=http://$ad/
     export https_proxy=http://$ad/
-    export ftp_proxy=http://$ad/
+    # export ftp_proxy=http://$ad/
     # export ALL_PROXY=socks5://$HostIP:$SocksPort/
 }
 
 unsetproxy() {
     unset https_proxy
     unset http_proxy
-    unset ftp_proxy
+    # unset ftp_proxy
     # unset ALL_PROXY
 }
 
+setproxy
+
 setv2ray() {
+    HttpPort=10809
     setproxy $HostIP:$HttpPort
+    showproxy
+}
+
+setclash() {
+    HttpPort=7890
+    setproxy $HostIP:$HttpPort
+    showproxy
 }
 
 setgithub() {
     setproxy 127.0.0.1:38457
+    showproxy
 }
-
-setv2ray
 
