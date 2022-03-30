486011d51079a3c086e0f407a406f686ab30b0de
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-03-30 11:31:48 +0800

    [emacs] use-package, which-key

--- emacs/init.el
+++ emacs/init.el
@@ -2,7 +2,7 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
+;; (setq gc-cons-threshold (* 50 1000 1000))
 
 ;; Profile emacs startup
 ; (add-hook 'emacs-startup-hook
@@ -11,15 +11,27 @@
 ;                      (emacs-init-time "%.2f")
 ;                      gcs-done)))
 
-; (require 'package)
+(require 'package)
+(setq package-enable-at-startup nil)
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 ; (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
 ;; Not load default.init shipped by OS or site
 ; (setq inhibit-default-init t)
 
-; (setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
 
 (global-linum-mode 1)
 ; (setq linum-format "%d| ")
@@ -44,3 +56,16 @@
 ;; (load-theme 'nord t)
 
 (put 'narrow-to-region 'disabled nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key use-package try)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
