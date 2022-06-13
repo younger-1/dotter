3c46862baafbcbf561078f808095e5e402e54251
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-06-13 11:33:30 +0800

    [emacs] company

---
+++ emacs/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-bullets which-key use-package try)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "SystemWindow" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Hack NF")))))
--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,9 @@
 ;; -*- lexical-binding: t; -*-
+;;; init.el  -*- lexical-binding: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
 
 ;; <https://github.com/daviwil/dotfiles/blob/master/Emacs.org>
 ;; <https://github.com/bling/dotemacs/blob/master/init.el>
+;; <https://github.com/mclear-tools/dotemacs/blob/master/init.el>
 
 ;; The default is 800 kilobytes.  Measured in bytes.
 ;; (setq gc-cons-threshold (* 50 1000 1000))
@@ -18,6 +20,8 @@
 ;;       (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
 ;;         (message "[Emacs initialized in %.3fs]" elapsed)))))
 
+;; (setq initial-buffer-choice (lambda () (help-with-tutorial) (get-buffer "TUTORIAL")))
+
 ;; Not load default.init shipped by OS or site
 ;; (setq inhibit-default-init t)
 
@@ -26,13 +30,13 @@
 (global-linum-mode 1)
 ;; (setq linum-format "%d| ")
 
-;; Hightlight
-(global-hl-line-mode 1)
+;; Hightlight current line
+;; (global-hl-line-mode 1)
 
 ;; Hide bars
-;; (menu-bar-mode 0)
-(tool-bar-mode 0)
-;; (scroll-bar-mode 0)
+;; (menu-bar-mode -1)
+(tool-bar-mode -1)
+;; (scroll-bar-mode -1)
 
 ;; (global-visual-line-mode)
 ;; (xterm-mouse-mode t)
@@ -40,6 +44,7 @@
 ;; (blink-cursor-mode -1)
 ;; (global-auto-revert-mode t)
 ;; (electric-indent-mode t)
+(electric-pair-mode t)
 ;; (transient-mark-mode t)
 (delete-selection-mode t)
 ;; (random t) ;; seed
@@ -49,6 +54,12 @@
 ;; (ido-mode 1)
 
 ;; (icomplete-mode 1)
+
+(setq tab-always-indent 'complete)
+
+(global-company-mode 1)
+(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0)
 
 ;; Show time
 ;; (display-time-mode 1)
@@ -60,8 +71,14 @@
 (defalias 'list-buffers 'ibuffer-other-window)
 ;; (defalias 'list-buffers 'ibuffer)
 
+;; (toggle-frame-maximized)
+;; (toggle-frame-fullscreen)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 (setq default-frame-alist (quote ((fullscreen . maximized))))
+
+;; Prevent Custom from modifying this file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror 'nomessage)
 
 (global-set-key (kbd "C-h C-q") 'view-emacs-FAQ)
 (global-set-key (kbd "C-h C-f") 'find-function)
@@ -74,6 +91,18 @@
   (find-file "~/.config/emacs/init.el"))
 
 (global-set-key (kbd "<f2>") 'open-init-file)
+
+;; <https://jao.io/blog/2021-09-08-high-signal-to-noise-emacs-command.html>
+(defun xy-buffer-same-mode (&rest modes)
+  "Pop to a buffer with a mode among MODES, or the current one if not given."
+  (interactive)
+  (let* ((modes (or modes (list major-mode)))
+         (pred (lambda (b)
+                 (let ((b (get-buffer (if (consp b) (car b) b))))
+                   (member (buffer-local-value 'major-mode b) modes)))))
+    (pop-to-buffer (read-buffer "Buffer: " nil t pred))))
+
+(global-set-key (kbd "<f3>") 'xy-buffer-same-mode)
 
 ;; (defun emacs-workflow-open ()
 ;;   ;(interactive)
@@ -91,6 +120,7 @@
 ;; (add-hook 'after-init-hook #'emacs-workflow-open)
 
 ;; (require 'package)
+;; Prevent package.el from modifying this file.
 (setq package-enable-at-startup nil)
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
@@ -112,18 +142,8 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+(use-package company
+  :ensure t)
+
 ;; To activate and use Nord Emacs as your default color theme load it in your init file:
 ;; (load-theme 'nord t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-bullets which-key use-package try)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "SystemWindow" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Hack NF")))))
